c370f706f7aeb939a2bca6fe5d670cb8b997b929
yashbhilala <yashbhilala41@gmail.com>
2025-01-09 03:48:38 +0530

    Initialized a new React Project

---
+++ frontend/movie_app/src/App.js
@@ -0,0 +1,94 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+
+
+  return (
+
+    <div>
+
+      Teri Mai Ka Bhook 
+    </div>
+  );
+
+
+
+
+
+}
+
+export default App;
+
+
+
+
+// To do - 
+
+// Questions 
+// 1) This will be a majorly purely front end based project, am I correct ?
+// 2) Could you please guide me on how to achieve sorting and filtering aspect of the project ?
+// 3) Could you please explain how the api can be used for infinite scroll ? [ EXPLAIN API ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
